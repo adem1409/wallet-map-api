1bd83d8d08b9de7a4c48bdf88f17d508f73360db
Adem Sammoudi <adem.sammoudi1409@gmail.com>
2024-12-09 19:35:08 +0100

    update profile

--- src/main/java/com/walletmap/api/controllers/UserController.java
+++ src/main/java/com/walletmap/api/controllers/UserController.java
@@ -8,34 +8,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.PathVariable;
-=======
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
->>>>>>> b6b321a65e27583033e1f4958a7f88d47b8386e4
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-<<<<<<< HEAD
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 import java.util.Optional;
-import com.walletmap.api.models.User;
-import com.walletmap.api.services.UserService;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-=======
-import org.springframework.web.multipart.MultipartFile;
 
 import com.walletmap.api.lib.AuthHelpers;
 import com.walletmap.api.lib.FileManager;
 import com.walletmap.api.models.User;
 import com.walletmap.api.services.UserService;
 
-import jakarta.servlet.http.HttpServletRequest;
->>>>>>> b6b321a65e27583033e1f4958a7f88d47b8386e4
+import jakarta.servlet.http.HttpServletRequestWrapper;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/users")
@@ -49,15 +41,55 @@ public class UserController {
         return userService.getAllUsers();
     }
 
-    @PostMapping("/edit/{id}")
-    public ResponseEntity<User> editUser(@PathVariable Long id, @RequestBody Map<String, String> updatedUser) {
-        System.out.println(updatedUser);
-        Optional<User> editedUser = userService.editUser(id, updatedUser);
+    @PostMapping("/edit")
+    public ResponseEntity<?> editUser(@RequestBody Map<String, String> updatedUser,
+            HttpServletRequestWrapper request) {
+
+        User currentUser = authHelpers.getAuthenticatedUser(request);
+
+        if (currentUser == null) {
+            return ResponseEntity.status(401).body("Not authenticated");
+        }
+
+        Optional<User> editedUser = userService.editUser(currentUser.getId(), updatedUser);
 
         if (editedUser.isPresent()) {
             return ResponseEntity.ok(editedUser.get());
         } else {
             return ResponseEntity.notFound().build();
+        }
+
+    }
+
+    @Autowired
+    private AuthHelpers authHelpers;
+
+    @PostMapping("/update-picture")
+    public ResponseEntity<?> updatePicture(
+            @RequestParam("picture") MultipartFile file,
+            HttpServletRequestWrapper request) {
+        try {
+
+            User currentUser = authHelpers.getAuthenticatedUser(request);
+
+            if (currentUser == null) {
+                return ResponseEntity.status(401).body("Not authenticated");
+            }
+
+            String uploadDir = "/uploads/profile-pictures/";
+
+            // Use FilesManager to upload the file
+            String uploadedFilePath = FileManager.uploadFile(file, uploadDir);
+            System.out.println("--------------- uploadedFilePath");
+            System.out.println(uploadedFilePath);
+
+            // Update the user's picture field
+            currentUser.setPicture(uploadedFilePath);
+            userService.saveUser(currentUser);
+
+            return ResponseEntity.ok(Map.of("message", "Photo uploaded successfully", "filePath", uploadedFilePath));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(Map.of("message", e.getMessage()));
         }
     }
 
@@ -77,56 +109,6 @@ public class UserController {
             return new ResponseEntity<>(savedUser, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(Map.of("message", e.getMessage()), HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @GetMapping("/test")
-    public String test(HttpServletRequest req) {
-        String path = System.getProperty("user.dir") + "/" + "test.txt";
-        System.out.println(path);
-        System.out.println("user -----------" + req.getAttribute("currentUser").toString());
-        return new String();
-    }
-
-    @Autowired
-    private AuthHelpers authHelpers;
-
-    @PostMapping("/update-picture")
-    public ResponseEntity<?> updatePicture(
-            @RequestParam("picture") MultipartFile file,
-            HttpServletRequest request) {
-        try {
-            // Fetch the authenticated user from request
-            // Optional<User> currentUserOptional = (Optional<User>)
-            // request.getAttribute("currentUser");
-            User currentUser = authHelpers.getAuthenticatedUser(request);
-
-            // if (currentUserOptional == null || currentUserOptional.isEmpty()) {
-            // return ResponseEntity.status(401).body("Not authenticated");
-            // }
-
-            if (currentUser == null) {
-                return ResponseEntity.status(401).body("Not authenticated");
-            }
-
-            // Get the User object from Optional
-            // User currentUser = currentUserOptional.get();
-
-            // Define the upload directory (e.g., "uploads/photos/")
-            String uploadDir = "/uploads/profile-pictures/";
-
-            // Use FilesManager to upload the file
-            String uploadedFilePath = FileManager.uploadFile(file, uploadDir);
-            System.out.println("--------------- uploadedFilePath");
-            System.out.println(uploadedFilePath);
-
-            // Update the user's picture field
-            currentUser.setPicture(uploadedFilePath);
-            userService.saveUser(currentUser);
-
-            return ResponseEntity.ok(Map.of("message", "Photo uploaded successfully", "filePath", uploadedFilePath));
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body(Map.of("message", e.getMessage()));
         }
     }
 
